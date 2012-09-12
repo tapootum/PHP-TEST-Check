27430d70880a8aa78516f3dd9678a6eec45ec458
Jarin Charoensiri <tapootumchannel@gmail.com>
2012-09-12 12:53:50 +0700

    explode substr

--- index.php
+++ index.php
@@ -1,66 +1,87 @@
 <html>
 <head>
-	<title>opendir()</title>
+	<title>Test Result</title>
 </head>
 <body>
-	<form action="index.php" method="get">
-		<?php
-		$objOpen = opendir("xml");
-		while (($file = readdir($objOpen)) !== false)
-		{	if (strlen($file) < 5 ){
+	<?php
+	$objOpen = opendir("xml");
+	while (($file = readdir($objOpen)) !== false)
+	{	
+		if (strlen($file) < 5 ){
+		}
+		else {
+		$name = explode(".",$file);
+		#echo $name[0] . "<br />";
+		$fname = substr($name[0],0,1);
+		#echo $fname . "<br />";
+		checkTest ($file,$fname);
+		}
+		
+	}		
+	function checkTest ($filename,$fname){
+		$name = "xml/" . $filename;
+		$ProgramList = array("InfraRecorder",
+			"Inkscape 0.48.2",
+			"Mozilla Thunderbird 11.0.1 (x86 en-US)",
+			"GIMP 2.6.11",
+			"PDFCreator V.1.3.2",
+			"7-Zip 9.20",
+			"Java(TM) 6 Update 29",
+			"Microsoft Visual C++ 2008 Redistributable - x86 9.0.30729.17",
+			"LibreOffice 3.3",
+			"OpenApp V.0.2.6.0",
+			"7-Zip 9.20 (x64 edition)",
+			"InfraRecorder 0.52 (x64 edition)",
+			"Microsoft Visual C++ 2008 Redistributable - x64 9.0.30729.17");
+			
+		$xml = simplexml_load_file($name);
+
+		$n=0;
+	#echo $xml->getName() . "<br />";
+		$osArch = 32;
+
+		foreach($xml->children() as $child){
+			for ($i=0; $i <= 12; $i++) { 				
+				if ($fname == "A") 
+				#if ($child == "7-Zip 9.20 (x64 edition)" || $child == "InfraRecorder 0.52 (x64 edition)" || $child == "Microsoft Visual C++ 2008 Redistributable - x64 9.0.30729.17")
+				{	
+					$osArch = 64; 
+				}
+				if ($child == $ProgramList[$i]){
+					$n++;
+					#echo $ProgramList[$i]. "<br />";
+				}
+			}	
+		}
+
+		echo '<a href="xml/' . $filename . '">' .$filename."</a>"."<br />";
+		#echo "           " . $n . "     " . $osArch;
+		if ($osArch == 32){
+			if ($n >= 9 )
+			{
+				echo '<img border="0" src="pass.jpg"/>'. "<br />";
+				#echo "Pass";
 			}
-			else {
-			echo '<a href="index.php?name='.$file.'">' . $file . "</a>" . "<br />";
+			else 
+			{
+				echo '<img border="0" src="fail.jpg"/>'. "<br />";
+				#echo "Fail";
 			}
 		}
-		?>
-	</form>
-	<br />
-	<br />
-	<br />
-	<br />
-
-
-	<?php
-	$name = "xml/" . $_GET['name'];
-	$ProgramList = array("InfraRecorder",
-		"Inkscape 0.48.2",
-		"Mozilla Thunderbird (8.0)",
-		"GIMP 2.6.11",
-		"PDFCreator",
-		"7-Zip 9.20",
-		"Java(TM) 6 Update 29",
-		"Microsoft Visual C++ 2008 Redistributable - x86 9.0.30729.17",
-		"LibreOffice 3.3",
-		"OpenApp",
-		"7-Zip 9.20 (x64 edition)",
-		"InfraRecorder 0.52 (x64 edition)",
-		"Microsoft Visual C++ 2008 Redistributable - x64 9.0.30729.17");
-
-	$xml = simplexml_load_file($name);
-
-	$n=0;
-	#echo $xml->getName() . "<br />";
-
-	foreach($xml->children() as $child){
-		for ($i=0; $i <= 12; $i++) { 
-			if ($child == $ProgramList[$i]){
-				$n++;
-  				#echo $ProgramList[$i]. "<br />";
+		else
+		{
+		if ($n >= 11)
+			{
+				echo '<img border="0" src="pass.jpg"/>'. "<br />";
+				#echo "Pass";
 			}
-		}	
-  #echo $child . "<br />";
-	}
-	echo $_GET['name']."<br />";
-	if ($n >= 9 )
-	{
-		echo '<img border="0" src="pass.jpg"/>';
-		echo "Pass";
-	}
-	else 
-	{
-		echo '<img border="0" src="fail.jpg"/>';
-		echo "Fail";
+			else 
+			{
+				echo '<img border="0" src="fail.jpg"/>'. "<br />";
+				#echo "Fail";
+			}
+		}
+		
 	}
 	?>
 </body>
